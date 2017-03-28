6ccc3969d25c988793cbd0b5f54cd63b2014d551
mellaniesoriano <msoriano808@gmail.com>
2017-03-27 21:44:32 -1000

    5 done

--- js/app.js
+++ js/app.js
@@ -26,6 +26,10 @@ function lessContent(){
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+function zoom(){
+  biggie.style.fontSize = "150%";
+}
+
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
